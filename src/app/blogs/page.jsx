af935211fd89ca431e3f65dec338aab5754b0ac4
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2024-05-12 12:30:05 +0530

    Created Company and blog Components

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className=" h-screen font-bold dark:bg-black dark:text-white flex justify-center items-center">
+      <div>
+        <h1 className=" text-[50px] text-center">BLOGS</h1>
+        <h1 className=" text-[50px] text-center">Work in PROGRESS...</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Company = () => {
   return (
-    <div>Company</div>
-  )
-}
+    <div className=" h-screen font-bold dark:bg-black dark:text-white flex justify-center items-center">
+      <div>
+      <h1 className=" text-[50px] text-center">COMPANY</h1>
+        <h1 className=" text-[50px]">Work in PROGRESS...</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Company
+export default Company;
